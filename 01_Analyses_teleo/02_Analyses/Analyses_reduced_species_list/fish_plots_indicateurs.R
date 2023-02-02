4dcfae1aaa582f76aaeb75dabeeec853f776426d
Alicia Dalongeville <alicia.dalongeville@gmail.com>
2023-02-02 15:22:38 +0100

    indicators analyses with reduced species list

---
+++ 01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/fish_plots_indicateurs.R
@@ -0,0 +1,185 @@
+## Librairies
+library(dplyr)
+library(rsq)
+library(fastDummies)
+library(tidyr)
+library(tidyverse)
+library(purrr)
+library(scales)
+library(margins)
+library(ggplot2)
+library(gridExtra)
+library(viridis)
+library(ggpubr)
+library(fishtree)
+library(multcomp)
+
+
+## Load data
+data <- read.csv("01_Analyses_teleo/00_data/reduced_matrice_teleo_port.csv", header=T, row.names=1) %>%
+  filter(rowSums(.) > 0) %>%
+  t(.)  %>%
+  as.data.frame(.)
+
+meta <- read.csv2("00_Metadata/metadata_port.csv", header=T)
+biohut <- meta %>% 
+  filter(type == "BIOHUT_port") %>%
+  pull(code_spygen)
+
+traits <- read.csv("01_Analyses_teleo/00_data/Functional_data_corrected_20220124.csv", header=T)
+
+ind_ports <- read.csv("01_Analyses_teleo/00_data/indicators_ports_2022_per_port_reduced_sp_list.csv", header=T, row.names=1) %>%
+  mutate(Location = "port") %>%
+  dplyr::filter(!rownames(.) %in% biohut)
+
+# Charger datas milieu naturel (= réserve et hors réserve)
+meta_nat <- read.csv2("00_Metadata/metadata_milieu_naturel.csv", header=T)
+ind_nat <- read.csv("01_Analyses_teleo/00_data/indicators_milieu_naturel.csv", header=T, row.names = 1) %>%
+  rownames_to_column(var="Sample") %>%
+  inner_join(meta_nat[,c("SPYGEN_code", "protection")], by=c("Sample"="SPYGEN_code")) %>%
+  dplyr::rename(Location = protection) %>%
+  column_to_rownames(var="Sample") %>%
+  dplyr::select(colnames(ind_ports))
+
+# Combiner les deux datasets
+ind_all <- rbind(ind_nat,ind_ports) %>%
+  mutate(DeBRa = log10(DeBRa))
+
+
+###############################################################################################
+## Plot indicator values for ports
+###############################################################################################
+
+################################################################################
+## Map of species richness for each port for the 2 campaigns
+################################################################################
+library(ggsn)
+library(ggmap)
+library(maps)
+library(magick)
+library(mapdata)
+library(tidyr)
+########################################################################################
+## MAP
+#########################################################################################
+R_tot <- read.csv("01_Analyses_teleo/00_data/Richness_total_port_reduced_sp_list.csv") %>%
+  mutate(Campaign ="Total") %>%
+  dplyr::rename(R = R_total,
+         Site = Port) %>%
+  dplyr::select(Site, Campaign, R)
+
+# Download the map for the Mediterranean Sea
+wH <- map_data("worldHires",  xlim=c(-8,37), ylim=c(29.5,47)) # subset polygons surrounding med sea
+
+## create map and save
+fig <- image_graph(width = 1200, height = 1100, res = 300)
+ggplot() +
+  geom_polygon(data = wH, aes(x=long, y = lat, group = group), 
+               alpha=0.7,fill = "gray80", color = NA) +
+  coord_fixed(xlim=c(3,7.3), ylim=c(42,44), ratio=1.2)+
+  #geom_point(aes(x = longitude_start_DD, y = latitude_start_DD), data=df, pch=21, col = "black") +
+  #scalebar(df, dist = 10, dist_unit = "km",
+  #         transform = TRUE, model = "WGS84",
+  #         st.bottom=T, location = "bottomright",
+  #         anchor = c(x=8.95,y=42.495),
+  #         st.dist = 0.05) +
+  labs(title = "Species richness") +
+  theme(panel.background = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major = element_blank(), #remove major gridlines
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.2),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        title = element_text(colour="black", size=7, face="bold"))
+dev.off()
+
+
+### put data together
+df <- ind_ports %>%
+  rownames_to_column(var="RowNames") %>%
+  separate(RowNames, c("Site", "Campaign"), "_", extra = "merge") %>%
+  mutate(Campaign = factor(Campaign, levels = c("October21", "June22"))) %>%
+  dplyr::select(Site, Campaign, R) %>%
+  bind_rows(R_tot) %>%
+  mutate(Campaign = factor(Campaign, levels = c("Total","October21", "June22"))) 
+
+
+
+# list of port names
+ports <- unique(df$Site)
+port_names <- ports
+port_names[7] <- "St.Maries Mer"
+
+## create list of the barplots
+bp <- list()
+
+for (i in 1: 7) {
+  to_plot <- df[which(df$Site %in% ports[i]),]
+  
+  bp[[i]]<-ggplot(data=to_plot, aes(x=Campaign, y=R, fill=Campaign)) +
+    geom_bar( position = 'dodge',stat="identity") +
+    scale_fill_manual(values=c("lightblue","lightgreen", "sandybrown")) +
+    ylim(0,78) +
+    theme(
+      panel.background = element_rect(fill='transparent'), #transparent panel bg
+      plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+      panel.grid.major = element_blank(), #remove major gridlines
+      panel.grid.minor = element_blank(), #remove minor gridlines 
+      legend.position="none") +
+    labs(y="Species richness",
+         title=port_names[i]) +
+    scale_x_discrete(labels=c("Total","Autumn", "Spring")) +
+    theme(axis.text.y=element_text(colour="black",size=7)) +
+    theme(axis.text.x=element_text(colour="black",size=7, angle=30, vjust=0.8)) +
+    theme(axis.title.y = element_blank()) +
+    theme(axis.title.x=element_blank())  +
+    theme(plot.title = element_text(colour="black", size=7, face="bold")) 
+  
+}
+names(bp) <- ports
+bp
+
+## create images with the barplots
+barfig1 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[1]]
+dev.off()
+
+barfig2 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[2]]
+dev.off()
+
+barfig3 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[3]]
+dev.off()
+
+barfig4 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[4]]
+dev.off()
+
+barfig5 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[5]]
+dev.off()
+
+barfig6 <- image_graph(width = 210, height = 250, res = 150, bg = 'transparent')
+bp[[6]]
+dev.off()
+
+barfig7 <- image_graph(width = 200, height = 220, res = 150, bg = 'transparent')
+bp[[7]]
+dev.off()
+
+final <- image_composite(fig, barfig1, offset = "+100+460")
+final <- image_composite(final, barfig2, offset = "+970+250") #
+final <- image_composite(final, barfig3, offset = "+620+480") #
+final <- image_composite(final, barfig4, offset = "+250+380")
+final <- image_composite(final, barfig5, offset = "+820+540") #
+final <- image_composite(final, barfig6, offset = "+100+650") #
+final <- image_composite(final, barfig7, offset = "+440+330") #
+final
+
+### Save the map
+image_write(final, "01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/Outputs/Map_Richesse_total.png", density=300)
+
---
+++ 01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM.R
@@ -0,0 +1,336 @@
+### Load the libraries
+library(dplyr)
+library(rsq)
+library(fastDummies)
+library(tidyr)
+library(tidyverse)
+library(purrr)
+library(scales)
+library(margins)
+library(ggplot2)
+library(viridis)
+library(spdep)
+library(ggpubr)
+library(gridExtra)
+library(spaMM)
+library(DHARMa)
+library(visreg)
+library(multcompView)
+library(multcomp)
+library(MuMIn)
+library(visreg)
+library(sjPlot)
+library(car)
+library(MASS)
+library(nlme)
+library(progressr)
+library(gplots)
+library(piecewiseSEM)
+library(emmeans)
+
+
+# load data ports
+ind_ports <- read.csv("01_Analyses_teleo/00_data/indicators_ports_2022_per_filter_reduced_sp_list.csv", header=T, row.names=1)
+
+# Charger datas milieu naturel (= réserve et hors réserve)
+meta_nat <- read.csv2("00_Metadata/metadata_milieu_naturel.csv", header=T)
+ind_nat <- read.csv("01_Analyses_teleo/00_data/indicators_milieu_naturel.csv", header=T, row.names = 1) %>%
+  rownames_to_column(var="Sample") %>%
+  inner_join(meta_nat, by=c("Sample"="SPYGEN_code")) %>%
+  dplyr::rename(type = protection,
+         Longitude = longitude_start_DD,
+         Latitude = latitude_start_DD) %>%
+  column_to_rownames(var="Sample") %>%
+  dplyr::select(colnames(ind_ports), "type", "Confinement", "Longitude", "Latitude") %>%
+  mutate(Confinement = case_when(Confinement == 'Y' ~ 'Lockdown',
+                         TRUE ~ 'Unlock'))
+
+### dataframe ports
+meta_ports <- read.csv2("00_Metadata/metadata_port.csv", header=T) %>%
+  filter(type == "Port")
+ind_ports <- ind_ports %>%
+  rownames_to_column(var="Sample") %>%
+  inner_join(meta_ports, by=c("Sample"="code_spygen")) %>%
+  mutate(Confinement = "Unlock") %>%
+  column_to_rownames(var="Sample") %>%
+  dplyr::select(colnames(ind_nat))
+
+# Combiner les deux datasets
+ind_all <- rbind(ind_nat,ind_ports) %>%
+  mutate(DeBRa = log10(DeBRa)) %>%
+  mutate_at('Confinement', as.factor)  %>%
+  mutate_at('Longitude', as.numeric)  %>%
+  mutate_at('Latitude', as.numeric) %>%
+  mutate(Reserve = case_when(type == 'reserve' ~ 'Y',
+                                 TRUE ~ 'N')) %>%
+  mutate(Port = case_when(type == 'Port' ~ 'Y',
+                             TRUE ~ 'N')) %>%
+  mutate_at('Reserve', as.factor) %>%
+  mutate_at('Port', as.factor) 
+
+
+ind_all$habitat <- factor(ind_all$type, levels=c("reserve", "outside", "Port"))
+ind_all$Confinement <- factor(ind_all$Confinement, labels=c("Unlock", "Lockdown"))
+
+ind_names <- c("Total species richness", "Cryptobenthic species richness", "Threatened species richness", "Commercial species richness")  
+
+
+### Matrix of spatial coordinates
+coords<- ind_all %>%
+  dplyr::select(Longitude, Latitude) %>%
+  as.matrix(.)
+
+
+
+###################################################################
+## Fit models
+###################################################################
+# Fit the model
+mod <- list()
+mod[[1]] <- fitme(R ~ Reserve * Confinement + Port + Matern(1|Longitude+Latitude),
+                  family = "gaussian",  data = ind_all)
+
+mod[[2]] <- fitme(Crypto ~ Reserve * Confinement + Port  + Matern(1|Longitude+Latitude),
+                  family = "gaussian",  data = ind_all)
+
+mod[[3]] <- fitme(RedList ~ Reserve * Confinement + Port  + Matern(1|Longitude+Latitude),
+                  family = "poisson",  data = ind_all)
+
+mod[[4]] <- fitme(Commercial ~ Reserve * Confinement + Port  + Matern(1|Longitude+Latitude),
+                  family = "gaussian",  data = ind_all)
+
+
+names(mod) <- ind_names
+saveRDS(mod, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/models_spaMM_indicators.RDS")
+
+
+
+
+#################################################################################
+## Get coefficients and conditional effects of covariates
+#################################################################################
+mod <- readRDS("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/models_spaMM_indicators.RDS")
+
+
+## post-fit evaluation
+## create the result dataframe
+coef <- matrix(0,5,8, dimnames=list(names(test_all$coef), paste(rep(names(mod),each=2), c("_coef", "_pval"))))
+R2 <- vector()
+
+
+# initiate indices
+c=1
+p=2
+
+for (i in 1:4) {
+  ### test of H_0: all regression coefficients are zero
+  test_all <- summary(glht(mod[[i]], coef.=fixef.HLfit))
+  coef[,c] <- test_all$coef
+  coef[,p] <- test_all$test$pvalues
+  
+  R2[i] <- cor(ind_all$R,predict(mod[[i]]))^2
+  
+  c=c+2
+  p=p+2
+}
+
+names(R2) <- ind_names
+
+write.csv(coef, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_variables_coefficients.csv")
+saveRDS(R2, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_R2.RDS")
+
+#############################################################################################
+### Partial effects
+##########################################################################################
+coef <- read.csv("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_variables_coefficients.csv", header=T, row.names=1)
+r2 <- readRDS("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_R2.RDS")
+
+pdep <- list()
+
+# Extract conditional effects of variables
+for (i in 1:4) {
+ pdep_port <- pdep_effects(mod[[i]],focal_var="Port")
+ pdep_port$focal_var <- c("out", "Port")
+ pdep_port$Confinement <- "Unlock"
+  
+  tmp <- ind_all %>%
+    mutate(Confinement = "Unlock")
+  
+  pdep_1 <- pdep_effects(mod[[i]],focal_var="Reserve", newdata=tmp)
+  pdep_1$focal_var <- c("outside", "reserve")
+  
+  tmp <- ind_all %>%
+    mutate(Confinement = "Lockdown")
+  
+  pdep_2 <- pdep_effects(mod[[i]],focal_var="Reserve", newdata=tmp)
+  pdep_2$focal_var <- c("outside", "reserve")
+  
+  pdep_1$Confinement <- "Lockdown" ; pdep_2$Confinement <- "Unlock"  
+  pdep[[i]] <- rbind(pdep_1,pdep_2, pdep_port[2,])
+  
+  
+}
+
+names(pdep) <-  names(mod)
+saveRDS(pdep, "01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_Conditional_effect_variables.RDS")
+
+#####################################################################################
+### Figures 3a 
+#####################################################################################
+pdep <- readRDS("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_Conditional_effect_variables.RDS")
+
+mytheme <- theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position="none",
+        axis.text.y=element_text(colour="black",size=14),
+        axis.text.x=element_text(colour="black",size=15),
+        axis.title.y=element_text(colour="black",size=14),
+        strip.text = element_text(size = 15),
+        axis.title.x=element_blank(),
+        plot.title = element_text(colour="black", size=15, face="bold", hjust=0.5),
+        plot.tag = element_text(colour="black", size=16, face="bold"),
+        plot.subtitle = element_text(colour="black", size=15,  hjust=0.5)) 
+
+# initiate plot list
+p <- list()
+
+## Loop to draw plots
+for (i in 1:4) {
+  v_i <- pdep[[i]]
+  v_i$focal_var <- factor(v_i$focal_var, levels=c("reserve", "outside", "Port"))
+  v_i$Confinement <- factor(v_i$Confinement, levels=c("Unlock", "Lockdown"))
+  
+  p[[i]] <- ggplot(v_i,aes(x=focal_var, y = pointp))+
+    #geom_jitter(data = v_i$res, mapping = aes(x = Protection, y = visregRes), colour="grey50", size=0.1) +
+    geom_crossbar(aes(ymin=low, ymax=up, fill = focal_var), 
+                  linetype=0, alpha=0.3) +
+    facet_wrap(~Confinement, scales="free_x") + # argument scales = "free_x" removes the empty factors (no Port during lockdown)
+    geom_errorbar(aes(ymin=pointp, ymax=pointp, colour = focal_var), 
+                  linetype=1, width=0.9, lwd=1.2) +
+    scale_colour_manual(values = c("darkblue", "cyan4",  "darkorange")) +
+    scale_fill_manual(values =  c("darkblue", "cyan4",  "darkorange")) +
+    labs(y="Species richness",
+         title=ind_names[i]) +
+    scale_x_discrete(labels = c("Reserve", "Fished", "Port")) +
+    mytheme +
+    labs(tag = paste("(", letters[i],")", sep="")) +
+    coord_cartesian(clip = "off")
+}
+
+
+## Save plot
+png("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/Outputs/Figure3a.png", 
+    width = 1200, height = 1000, ) 
+do.call(grid.arrange,c(p, list(ncol=2)))
+dev.off()
+
+
+#################################################################################################################
+## Model testing effects of port characteristics on indicators
+#################################################################################################################
+
+meta <- read.csv2("00_Metadata/metadata_port.csv", header=T)  %>%
+  filter(type != "BIOHUT_port")
+
+ind_ports <- read.csv("01_Analyses_teleo/00_data/indicators_ports_2022_per_filter_reduced_sp_list.csv", header=T, row.names=1) %>%
+  rownames_to_column(var="code_spygen")
+
+mydata <- ind_ports %>%
+  inner_join(meta, by="code_spygen") %>%
+  # transform all character columns to factors
+  mutate_if(sapply(., is.character), as.factor) %>%
+  mutate_at(c("Longitude", "Latitude", "Area_ha", "Depth_m"), as.numeric)
+
+### Create a data set of response variables (indicators)
+Y <- mydata %>%
+  dplyr::select(R,Crypto,RedList, Commercial) %>%
+  # express vqriables as proportion of species richness
+  mutate(across(c(Crypto, RedList,  Commercial), ~ .x/R)) 
+
+### Create a data set of explanatory variables (environment + protection)
+X <- mydata %>%
+  dplyr::select(Certification, Campaign, Area_ha, Depth_m, Habitat ) %>%
+  # transform all character columns to factors
+  mutate_if(sapply(., is.character), as.factor)
+
+### Create a vector of site names
+sites <- mydata %>%
+  dplyr::pull(site)
+
+
+### Matrix of spatial coordinates
+coords<- mydata %>%
+  dplyr::select(Longitude, Latitude) %>%
+  as.matrix(.)
+
+### Create a vector of indicator names
+ind_names <- c("Total species richness", "Cryptobenthics species richness", 
+               "Threatened species richness", "Commercial species richness")  
+
+
+# Create a vector of distributions : Poisson for RedList and Chondri, 
+# and Gaussian for all the others
+distri <- c("gaussian", "gaussian", 
+            "quasipoisson", "gaussian")
+
+###################################################################
+## Fit models
+###################################################################
+# Fit the model
+mod <- list()
+mod[[1]] <- fitme(R ~ Certification + Campaign + Area_ha + Depth_m + Habitat +
+                    Matern(1|Longitude+Latitude),
+                  family = "gaussian",  data = mydata)
+
+mod[[2]] <- fitme(Crypto ~ Certification + Campaign + Area_ha + Depth_m + Habitat +
+                    Matern(1|Longitude+Latitude),
+                           family = "gaussian",  data = mydata)
+                  
+mod[[3]] <- fitme(RedList ~ Certification + Campaign + Area_ha + Depth_m + Habitat +
+                     Matern(1|Longitude+Latitude),
+                             family = "poisson",  data = mydata)
+                                    
+mod[[4]] <- fitme(Commercial ~ Certification + Campaign + Area_ha + Depth_m + Habitat +
+                     Matern(1|Longitude+Latitude),
+                             family = "gaussian",  data = mydata)
+                                                      
+                                                      
+names(mod) <- ind_names
+saveRDS(mod, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/models_spaMM_indicators_ports.RDS")
+                                                      
+                                                      
+
+#################################################################################
+## Get coefficients and conditional effects of covariates
+#################################################################################
+mod <- readRDS("01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/models_spaMM_indicators_ports.RDS")
+
+
+## post-fit evaluation
+## create the result dataframe
+coef <- matrix(0,6,8, dimnames=list(names(test_all$coef), paste(rep(names(mod),each=2), c("_coef", "_pval"))))
+R2 <- vector()
+
+
+# initiate indices
+c=1
+p=2
+
+for (i in 1:4) {
+  ### test of H_0: all regression coefficients are zero
+  test_all <- summary(glht(mod[[i]], coef.=fixef.HLfit))
+  coef[,c] <- test_all$coef
+  coef[,p] <- test_all$test$pvalues
+  
+  R2[i] <- cor(mydata$R,predict(mod[[i]]))^2
+  
+  c=c+2
+  p=p+2
+}
+
+names(R2) <- ind_names
+
+write.csv(coef, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_variables_coefficients_ports.csv")
+saveRDS(R2, file="01_Analyses_teleo/02_Analyses/Analyses_reduced_species_list/spaMM_indicators_R2_ports.RDS")
+
