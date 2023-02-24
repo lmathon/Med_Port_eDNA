35faafcb0268121e2278a124a10fdec0eadba44f
moggob <morganebruno@orange.fr>
2023-02-24 17:40:34 +0100

    refactor(ddRDA_all.R): add plot_grda_sites function

--- 01_Analyses_teleo/02_Analyses/dbRDA_all.R
+++ 01_Analyses_teleo/02_Analyses/dbRDA_all.R
@@ -43,9 +43,38 @@ beta_nes <- beta$beta.jne
 
 
 ########################################################################################################################
+#### FUNCTION PLOT GRDA
+plot_grda_sites <- function(df, fill_values = c("#7FC5C5","#FFC47E", "#7F7FC5")){
+  grda_sites <- ggplot(df, aes(x= CAP1, y = CAP2)) +
+    geom_hline(yintercept = 0, lty = 2, col = "grey") + # ligne horizontqle
+    geom_vline(xintercept = 0, lty = 2, col = "grey") + # ligne verticale
+    geom_encircle(aes(group = habitat,linetype = habitat,fill= habitat), s_shape = 1, expand = 0,
+                  alpha = 0.6, show.legend = FALSE) + # hull area 
+    geom_point(aes(pch = habitat, fill = habitat), cex = 3, col = "black") +
+    #ylim(-3.7, 2.4) + # limites axes Y
+    scale_fill_manual(values = as.vector(fill_values),
+                      name = "", labels = c("Fished", "Port", "Reserve")) +
+    scale_shape_manual(values = c(22,21,23),
+                       name = "", labels = c("Fished", "Port", "Reserve")) +
+    labs(x = paste0("CAP1 (", CAP1, "%)"), y = paste0("CAP2 (", CAP2, "%)"),
+         title = "") +
+    theme_bw() +
+    guides(fill=guide_legend(ncol=3)) +
+    theme(axis.line = element_line(colour = "black"),
+          axis.text = element_text(size =13, colour = "gray25"),
+          axis.title = element_text(size = 14, colour = "gray25"),
+          legend.position = c(1, 0),             # position in bottom right corner
+          legend.justification = c(1, 0),        # correct legend justificaton
+          legend.box.margin=margin(c(0,1,1,1)),  # add margin as to not overlap with axis box
+          legend.title = element_blank(),
+          legend.text = element_text(size=18),
+          legend.box.background=element_rect(fill='transparent', colour='transparent'),
+          panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+          panel.background = element_rect(colour = "black", size=1)) 
+}
+
 #### Compute dbRDA on total beta-diversity ####
 RDA_all<- capscale(beta_tot ~ habitat + Condition(Longitude), meta_dbrda)
-
 
 ### Test the significance
 anova(RDA_all, perm=999)
@@ -66,45 +95,13 @@ CAP2 <- round(sumdbrda$cont$importance["Proportion Explained", "CAP2"]*100, 1)
 identical(as.character(meta_dbrda$code_spygen), rownames(site_scores)) # verify that data in same order
 site_scores_groups <- cbind(site_scores,select(meta_dbrda, habitat))
 
-# vecteur de couleurs
-mycol = c("#7FC5C5","#FFC47E", "#7F7FC5")
-
 ### plot sites 
-grda_sites <- ggplot(site_scores_groups, aes(x= CAP1, y = CAP2)) +
-  geom_hline(yintercept = 0, lty = 2, col = "grey") + # ligne horizontqle
-  geom_vline(xintercept = 0, lty = 2, col = "grey") + # ligne verticale
-  geom_encircle(aes(group = habitat,linetype = habitat,fill= habitat), s_shape = 1, expand = 0,
-                alpha = 0.6, show.legend = FALSE) + # hull area 
-  geom_point(aes(pch = habitat, fill = habitat), cex = 3, col = "black") +
-  #ylim(-3.7, 2.4) + # limites axes Y
-  scale_fill_manual(values = as.vector(mycol),
-                    name = "", labels = c("Fished", "Port", "Reserve")) +
-  scale_shape_manual(values = c(22,21,23),
-                     name = "", labels = c("Fished", "Port", "Reserve")) +
-  labs(x = paste0("CAP1 (", CAP1, "%)"), y = paste0("CAP2 (", CAP2, "%)"),
-       title = "") +
-  theme_bw() +
-  guides(fill=guide_legend(ncol=3)) +
-  theme(axis.line = element_line(colour = "black"),
-        axis.text = element_text(size =13, colour = "gray25"),
-        axis.title = element_text(size = 14, colour = "gray25"),
-        legend.position = c(1, 0),             # position in bottom right corner
-        legend.justification = c(1, 0),        # correct legend justificaton
-        legend.box.margin=margin(c(0,1,1,1)),  # add margin as to not overlap with axis box
-        legend.title = element_blank(),
-        legend.text = element_text(size=18),
-        legend.box.background=element_rect(fill='transparent', colour='transparent'),
-        panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        panel.background = element_rect(colour = "black", size=1)) 
+grda_sites <- plot_grda_sites(df = site_scores_groups)
 grda_sites
-
 
 # export figure
 ggsave(plot = grda_sites, filename = "01_Analyses_teleo/03_Outputs/dbRDA_totale.jpeg", 
        width = 7,  height = 7, dpi = 600)
-
-
-
 
 ########################################################################################################################
 #### Compute dbRDA on turnover beta-diversity ####
@@ -124,38 +121,9 @@ CAP2 <- round(sumdbrda$cont$importance["Proportion Explained", "CAP2"]*100, 1)
 identical(as.character(meta_dbrda$code_spygen), rownames(site_scores)) # verify that data in same order
 site_scores_groups <- cbind(site_scores,select(meta_dbrda, habitat))
 
-# vecteur de couleurs
-mycol = c("#7FC5C5","#FFC47E", "#7F7FC5")
-
-### plot sites 
-grda_sites <- ggplot(site_scores_groups, aes(x= CAP1, y = CAP2)) +
-  geom_hline(yintercept = 0, lty = 2, col = "grey") + # ligne horizontqle
-  geom_vline(xintercept = 0, lty = 2, col = "grey") + # ligne verticale
-  geom_encircle(aes(group = habitat,linetype = habitat,fill= habitat), s_shape = 1, expand = 0,
-                alpha = 0.6, show.legend = FALSE) + # hull area 
-  geom_point(aes(pch = habitat, fill = habitat), cex = 3, col = "black") +
-  #ylim(-3.7, 2.4) + # limites axes Y
-  scale_fill_manual(values = as.vector(mycol),
-                    name = "", labels = c("Fished", "Port", "Reserve")) +
-  scale_shape_manual(values = c(22,21,23),
-                     name = "", labels = c("Fished", "Port", "Reserve")) +
-  labs(x = paste0("CAP1 (", CAP1, "%)"), y = paste0("CAP2 (", CAP2, "%)"),
-       title = "") +
-  theme_bw() +
-  guides(fill=guide_legend(ncol=3)) +
-  theme(axis.line = element_line(colour = "black"),
-        axis.text = element_text(size =13, colour = "gray25"),
-        axis.title = element_text(size = 14, colour = "gray25"),
-        legend.position = c(1, 0),             # position in bottom right corner
-        legend.justification = c(1, 0),        # correct legend justificaton
-        legend.box.margin=margin(c(0,1,1,1)),  # add margin as to not overlap with axis box
-        legend.title = element_blank(),
-        legend.text = element_text(size=18),
-        legend.box.background=element_rect(fill='transparent', colour='transparent'),
-        panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        panel.background = element_rect(colour = "black", size=1)) 
+### plot sites
+grda_sites <- plot_grda_sites(df = site_scores_groups)
 grda_sites
-
 
 # export figure
 ggsave(plot = grda_sites, filename = "01_Analyses_teleo/03_Outputs/dbRDA_totale_turnover.jpeg", 
@@ -180,41 +148,10 @@ CAP2 <- round(sumdbrda$cont$importance["Proportion Explained", "CAP2"]*100, 1)
 identical(as.character(meta_dbrda$code_spygen), rownames(site_scores)) # verify that data in same order
 site_scores_groups <- cbind(site_scores,select(meta_dbrda, habitat))
 
-# vecteur de couleurs
-mycol = c("#7FC5C5","#FFC47E", "#7F7FC5")
-
 ### plot sites 
-grda_sites <- ggplot(site_scores_groups, aes(x= CAP1, y = CAP2)) +
-  geom_hline(yintercept = 0, lty = 2, col = "grey") + # ligne horizontqle
-  geom_vline(xintercept = 0, lty = 2, col = "grey") + # ligne verticale
-  geom_encircle(aes(group = habitat,linetype = habitat,fill= habitat), s_shape = 1, expand = 0,
-                alpha = 0.6, show.legend = FALSE) + # hull area 
-  geom_point(aes(pch = habitat, fill = habitat), cex = 3, col = "black") +
-  #ylim(-3.7, 2.4) + # limites axes Y
-  scale_fill_manual(values = as.vector(mycol),
-                    name = "", labels = c("Fished", "Port", "Reserve")) +
-  scale_shape_manual(values = c(22,21,23),
-                     name = "", labels = c("Fished", "Port", "Reserve")) +
-  labs(x = paste0("CAP1 (", CAP1, "%)"), y = paste0("CAP2 (", CAP2, "%)"),
-       title = "") +
-  theme_bw() +
-  guides(fill=guide_legend(ncol=3)) +
-  theme(axis.line = element_line(colour = "black"),
-        axis.text = element_text(size =13, colour = "gray25"),
-        axis.title = element_text(size = 14, colour = "gray25"),
-        legend.position = c(1, 0),             # position in bottom right corner
-        legend.justification = c(1, 0),        # correct legend justificaton
-        legend.box.margin=margin(c(0,1,1,1)),  # add margin as to not overlap with axis box
-        legend.title = element_blank(),
-        legend.text = element_text(size=18),
-        legend.box.background=element_rect(fill='transparent', colour='transparent'),
-        panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        panel.background = element_rect(colour = "black", size=1)) 
+grda_sites <- plot_grda_sites(df = site_scores_groups)
 grda_sites
-
 
 # export figure
 ggsave(plot = grda_sites, filename = "01_Analyses_teleo/03_Outputs/dbRDA_totale_nestedness.jpeg", 
        width = 7,  height = 7, dpi = 600)
-
-
